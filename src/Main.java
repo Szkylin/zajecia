126ac235f8f65b2deea28df0961822c8e43f44b6
Szkylin <adriangoncz02@gmail.com>
2023-01-06 13:22:29 +0100

    Zadanie 1

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Animal dog = new Animal("canis");
+        Animal cat = new Animal("felis");
+        dog.name = "Burek";
+        dog.setWeight(50.0);
+        dog.age = 9;
+        cat.name = "Mruczek";
+        cat.setWeight(5.0);
+        cat.age = 5;
+
+        dog.takeForWalk();
+        dog.feed();
+        cat.takeForWalk();
+        cat.feed();
+
+    }
+}
